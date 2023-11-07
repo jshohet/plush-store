a051168b282743426c1b2dbafa92f7fcca01e0b6
jshohet <parykeeth90@gmail.com>
2023-11-07 14:11:07 -0500

    viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import { GlobalContextProvider } from './assets/store/store'
 export const metadata: Metadata = {
   title: 'Chew Plush Store',
   description: 'Plushie store for all your plushie needs.',
+  viewport:{
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  }
 }
 
 export default function RootLayout({
